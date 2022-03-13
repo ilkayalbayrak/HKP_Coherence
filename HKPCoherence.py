1c6cb320cf48326440488e3eed7429bd00230e8c
ilkayalbayrak <ilkayyalbayrak@gmail.com>
2022-03-13 23:58:15 +0100

    added solution to find minimal moles

--- HKPCoherence.py
+++ HKPCoherence.py
@@ -1,3 +1,6 @@
+import numpy as np
+import time
+
 '''
     #######- GREEDY ALGORITHM -#######
     A public item must be suppressed, if the item on ITS OWN IS A MOLE.
@@ -7,6 +10,14 @@
 
 
 '''
+
+
+def find_unique_items(input_file):
+    with open(input_file, "r") as file:
+        lines = file.read()
+        unique_items = set(lines.split())
+
+        return unique_items
 
 
 # this can be an inner class of hkp coherence
@@ -38,6 +49,7 @@ class HKPCoherence:
         self.k = k
         self.p = p
         self.transactions = list()
+        self.size1_moles = list()
         for index, row in enumerate(self.dataset):
             # TODO:
             public = [i for i in row if i not in private_item_list]
@@ -94,14 +106,16 @@ class HKPCoherence:
     def suppress_size1_moles(self):
         """
         # suppress all the public items that are size-1 moles
-        :return:
+        :return: list of size1 moles (just in case I have use for them down the line)
         """
-        removed_items = list()
+        # size1_moles = list()
+        start_time = time.time()
+        print("Started suppressing size-1 moles")
         for e in self.public_item_list:
 
             if self.is_mole([e]):
                 # delete item e from all transactions
-                removed_items.append(e)
+                self.size1_moles.append(e)
                 for t in self.transactions:
                     if e in t.public:
                         t.public.remove(e)
@@ -109,4 +123,114 @@ class HKPCoherence:
                     #     t.public.remove(e)
                     # except ValueError:
                     #     continue
-        print(f"Suppressed {len(removed_items)} size-1 mole public items.")
+        print(
+            f"Suppressed {len(self.size1_moles)} size-1 mole public items. Time passed: {int(time.time() - start_time)}")
+
+    # MM(e) is the number of minimal moles containing the item e
+    def find_minimal_moles(self):
+
+        start_time = time.time()
+        print("Started identification process for Minimal moles and Extendible non-moles")
+        # C1 initial candidates
+        # M1 init minimal moles
+        # F1 init extendible non-moles
+
+        C1 = [i for i in self.public_item_list if i not in self.size1_moles]
+        M1 = list()
+        F1 = list()
+
+        for e in C1:
+            if self.is_mole([e]):
+                M1.append([e])
+            else:
+                F1.append([e])
+
+        F = [F1]
+        M = [M1]
+        del F1, M1, C1
+        i = 0
+        while i < self.p and len(F[i]) > 0:
+            print(f"i:{i} and len(F[i]):{len(F[i])}")
+            # generate Canditate set for Mi+1 and Fi+1
+            F_, M_ = self.foo(F[i], M[i])
+            F.append(F_)
+            M.append(M_)
+            i += 1
+
+            # scan D
+
+        # print(f"M:{len(M)}, F1:{len(F)}")
+        # return M1, F1
+
+    @staticmethod
+    def diff_list(L1, L2):
+        return len(set(L1).symmetric_difference(set(L2)))
+
+    @staticmethod
+    def generate_C( F:list, M:list) -> list:
+        """
+
+        :param F: List of extendible moles
+        :param M: List of minimal moles
+        :return: Candidate list Ci+1
+        """
+        # this is basically a list of betas
+        # for Fi and Fi+1
+
+        C = list()
+        for i in range(len(F)):
+            for j in range(i + 1, len(F)):
+                # print(f"Fi:{F[i]}, Fi+1:{F[j]}")
+                # if self.diff_list(F[i], F[j]) == 2:
+                new_F = list(F[i])
+                new_F.extend(x for x in F[j] if x not in new_F)
+                flag = False
+                for m in M:
+                    if set(new_F).issuperset(m):
+                        flag = True
+                        break
+                if not flag:
+                    C.append(new_F)
+        return C
+
+    def foo(self, F, M):
+        """Return (Fi+1, Mi+1)
+        """
+        M1 = list()
+        F1 = list()
+        C1 = self.generate_C(F, M)
+        for beta in C1:
+            if self.is_mole(beta):
+                M1.append(beta)
+            else:
+                F1.append(beta)
+        return F1, M1
+
+    def pipeline(self):
+        pass
+
+
+if __name__ == "__main__":
+    DATA_PATH = "./Dataset/T40I10D100K_100.txt"
+
+    # find unique items to randomly choose the private items among them
+    unique_items = find_unique_items(DATA_PATH)
+    unique_items = [int(i) for i in unique_items]
+    print(f"Number of Unique items: {len(unique_items)}")
+
+    # randomly choose private items
+    # dataset =
+    np.random.seed(42)
+    private_items = np.random.choice(unique_items, replace=False, size=10)
+    public_items = [i for i in unique_items if i not in private_items]
+    print(f"private: {len(private_items)}\npublic: {len(public_items)}")
+
+    dataset = []
+    with open(DATA_PATH, "r") as file:
+        for line in file:
+            dataset.append([int(i) for i in set(line.rstrip().split())])
+
+    hkp = HKPCoherence(dataset, public_items, private_items, h=0.8, k=3, p=4)
+
+    hkp.suppress_size1_moles()
+    hkp.find_minimal_moles()
